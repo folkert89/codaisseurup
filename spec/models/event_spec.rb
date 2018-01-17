a270c102d24237b4b0088cdf41a3cf225aab2db8
folkert89 <folkertburghout@hotmail.com>
2018-01-17 18:44:00 +0100

    Add model specs: associations

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -47,9 +47,34 @@ RSpec.describe Event, type: :model do
     let!(:event2) { create :event, price: 200 }
     let!(:event3) { create :event, price: 50 }
 
-    it "returns a sorted array of rooms by prices" do
+    it "returns a sorted array of events by prices" do
       # note that they should not come out in the order that they were created
       expect(Event.order_by_price).to eq([event3, event1, event2])
     end
+  end
+  describe "association with user" do
+    let(:user) { create :user }
+
+    it "belongs to a user" do
+      event = user.events.build(name: "Sailing")
+
+      expect(event.user).to eq(user)
+    end
+  end
+  describe "association with category" do
+    let(:event) { create :event }
+
+    let(:category1) { create :category, name: "Bright", events: [event] }
+    let(:category2) { create :category, name: "Clean lines", events: [event] }
+    let(:category3) { create :category, name: "A Man's Touch", events: [event] }
+
+    it "has categories" do
+      expect(event.categories).to include(category1)
+      expect(event.categories).to include(category2)
+      expect(event.categories).to include(category3)
+    end
+    #
+    # # Or we do it the shoulda way:
+    # it { is_expected.to have_and_belong_to_many :categories }
   end
 end
